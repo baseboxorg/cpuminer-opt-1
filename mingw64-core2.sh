25c0b9002cc323a337fc94e20cac77571362b378
Felix Brucker <Felix Brucker>
2016-10-20 01:26:03 +0200

    newer gcc version, ignore this

--- mingw64-core2.sh
+++ mingw64-core2.sh
@@ -7,7 +7,7 @@ CURL_PREFIX=/usr/local
 SSL_PREFIX=/usr/local/ssl
 
 # gcc 4.4
-extracflags="-O3 -Wall -D_REENTRANT -fmerge-all-constants" # -funroll-loops -fvariable-expansion-in-unroller -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
+extracflags="-O3 -march=core2 -Wall -D_REENTRANT -fmerge-all-constants" # -funroll-loops -fvariable-expansion-in-unroller -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
 
 # gcc 4.8+
 # extracflags="$extracflags -Ofast -fuse-linker-plugin -ftree-loop-if-convert-stores" # -flto "
@@ -17,10 +17,10 @@ CFLAGS="-DCURL_STATICLIB -DOPENSSL_NO_ASM -DUSE_ASM $extracflags"
 # CPPFLAGS=""
 
 # icon
-windres res/icon.rc icon.o
+x86_64-w64-mingw32-windres res/icon.rc icon.o
 
-./configure --build=x86_64-w64-mingw32 -march=core2 --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX \
-	CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" LDFLAGS="icon.o"
+./configure --build=x86_64-w64-mingw32 --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX \
+	CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS -std=gnu++11" LDFLAGS="icon.o"
 
 make
 
--- mingw64-westmere.sh
+++ mingw64-westmere.sh
@@ -7,7 +7,7 @@ CURL_PREFIX=/usr/local
 SSL_PREFIX=/usr/local/ssl
 
 # gcc 4.4
-extracflags="-O3 -Wall -D_REENTRANT -fmerge-all-constants" # -funroll-loops -fvariable-expansion-in-unroller -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
+extracflags="-O3 -march=westmere -Wall -D_REENTRANT -fmerge-all-constants" # -funroll-loops -fvariable-expansion-in-unroller -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
 
 # gcc 4.8+
 # extracflags="$extracflags -Ofast -fuse-linker-plugin -ftree-loop-if-convert-stores" # -flto "
@@ -19,8 +19,8 @@ CFLAGS="-DCURL_STATICLIB -DOPENSSL_NO_ASM -DUSE_ASM $extracflags"
 # icon
 x86_64-w64-mingw32-windres res/icon.rc icon.o
 
-./configure --build=x86_64-w64-mingw32 -march=westmere --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX \
-	CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" LDFLAGS="icon.o"
+./configure --build=x86_64-w64-mingw32 --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX \
+	CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS -std=gnu++11" LDFLAGS="icon.o"
 
 make
 
