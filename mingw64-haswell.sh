6442f305e11cc68d8e95c7ada33cd1c9165f5212
Felix Brucker <Felix Brucker>
2016-10-20 02:05:41 +0200

    newer gcc version, ignore this

--- mingw64-haswell.sh
+++ mingw64-haswell.sh
@@ -1,26 +1,14 @@
+#!/bin/bash
+
 make clean || echo clean
 
 rm -f config.status
 ./autogen.sh || echo done
 
-CURL_PREFIX=/usr/local
-SSL_PREFIX=/usr/local/ssl
-
-# gcc 4.4
-extracflags="-O3 -march=haswell -Wall -D_REENTRANT -fmerge-all-constants" # -funroll-loops -fvariable-expansion-in-unroller -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
-
-# gcc 4.8+
-# extracflags="$extracflags -Ofast -fuse-linker-plugin -ftree-loop-if-convert-stores" # -flto "
-
-# extracflags="-pg -static -fno-inline-small-functions"
-CFLAGS="-DCURL_STATICLIB -DOPENSSL_NO_ASM -DUSE_ASM $extracflags"
-# CPPFLAGS=""
-
 # icon
 x86_64-w64-mingw32-windres res/icon.rc icon.o
 
-./configure --build=x86_64-w64-mingw32 --with-crypto=$SSL_PREFIX --with-curl=$CURL_PREFIX \
-	CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS -std=gnu++11" LDFLAGS="icon.o"
+CFLAGS="-O3 -march=core-avx2 -Wall" CXXFLAGS="$CFLAGS -std=gnu++11 -fpermissive" LDFLAGS="icon.o -static" ./configure --with-curl
 
 make
 
